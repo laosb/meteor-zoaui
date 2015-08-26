2657a23eb58ce6c6a6c96e16a6e57bb28b2e73b4
laosb <laosb@lasteam.net>
2015-08-26 16:47:02 +0800

    Typo fix & really published.

--- package.js
+++ package.js
@@ -10,21 +10,21 @@ Package.onUse(function(api) {
     api.versionsFrom('1.1.0.3');
 
     //File oreder in the recommened way: http://infinnie.github.io/zoaui/css/
-    api.addFile('zoaui/css/fonts.css');
-    api.addFile('zoaui/css/general.css');
-    api.addFile('zoaui/css/layout.css');
-    api.addFile('zoaui/css/typography.css');
-    api.addFile('zoaui/css/button.css');
-    api.addFile('zoaui/css/nav.css');
-    api.addFile('zoaui/css/form.css');
-    api.addFile('zoaui/css/dialog.css');
-    api.addFile('zoaui/css/dropdown.css');
-    api.addFile('zoaui/css/triangle.css');
-    api.addFile('zoaui/css/pagination.css');
-    api.addFile('zoaui/css/image.css');
+    api.addFiles('zoaui/css/fonts.css');
+    api.addFiles('zoaui/css/general.css');
+    api.addFiles('zoaui/css/layout.css');
+    api.addFiles('zoaui/css/typography.css');
+    api.addFiles('zoaui/css/button.css');
+    api.addFiles('zoaui/css/nav.css');
+    api.addFiles('zoaui/css/form.css');
+    api.addFiles('zoaui/css/dialog.css');
+    api.addFiles('zoaui/css/dropdown.css');
+    api.addFiles('zoaui/css/triangle.css');
+    api.addFiles('zoaui/css/pagination.css');
+    api.addFiles('zoaui/css/image.css');
 
     api.use('jquery');
 
-    api.addFile('zoaui/js/nav.js', 'client');
-    api.addFile('zoaui/js/toggle.js', 'client');
+    api.addFiles('zoaui/js/nav.js', 'client');
+    api.addFiles('zoaui/js/toggle.js', 'client');
 });
