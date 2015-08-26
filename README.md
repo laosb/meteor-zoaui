73d02bbebbe27844c4e09710ad2f4dcb91bd033c
laosb <laosb@lasteam.net>
2015-08-26 16:28:36 +0800

    0.1.7

---
+++ README.md
@@ -0,0 +1,8 @@
+Zoa UI for Meteor
+=================
+
+A wrapper package for [Zoa UI](http://infinnie.github.io/zoaui/).
+
+This package is made for and by LyuYea @ UnitedHubs.
+
+Thanks to @infinnie.
--- package.js
+++ package.js
@@ -1,22 +1,30 @@
 Package.describe({
-  name: 'meteor-zoaui',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+    name: 'zoa:ui',
+    version: '0.1.7',
+    summary: 'Wrapper for Zoa UI.',
+    git: 'https://github.com/laosb/meteor-zoaui',
+    documentation: 'README.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.1.0.3');
-  api.addFiles('meteor-zoaui.js');
-});
+    api.versionsFrom('1.1.0.3');
 
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('meteor-zoaui');
-  api.addFiles('meteor-zoaui-tests.js');
+    //File oreder in the recommened way: http://infinnie.github.io/zoaui/css/
+    api.addFile('zoaui/css/fonts.css');
+    api.addFile('zoaui/css/general.css');
+    api.addFile('zoaui/css/layout.css');
+    api.addFile('zoaui/css/typography.css');
+    api.addFile('zoaui/css/button.css');
+    api.addFile('zoaui/css/nav.css');
+    api.addFile('zoaui/css/form.css');
+    api.addFile('zoaui/css/dialog.css');
+    api.addFile('zoaui/css/dropdown.css');
+    api.addFile('zoaui/css/triangle.css');
+    api.addFile('zoaui/css/pagination.css');
+    api.addFile('zoaui/css/image.css');
+
+    api.use('jquery');
+
+    api.addFile('zoaui/js/nav.js', 'client');
+    api.addFile('zoaui/js/toggle.js', 'client');
 });
